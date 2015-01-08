bd4e17d0c8923f6a1d49da9d4153af3d38199f43
riceknight <kitkelly@gmail.com>
2015-01-08 16:54:29 +1100

    Create two ConnectionDetector Classes to see which works better. Eventually only one will be used.

---
+++ mobile/src/main/java/com/riceknight/sunshield/ConnectionDetector.java
@@ -0,0 +1,33 @@
+package com.riceknight.sunshield;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+
+/**
+ * Created by riceknight on 21/12/2014.
+ */
+public class ConnectionDetector {
+
+    private Context _context;
+
+    public ConnectionDetector(Context context) {
+        this._context = context;
+    }
+
+    public boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null)
+        {
+            NetworkInfo[] info = cm.getAllNetworkInfo();
+            if (info != null)
+                for(int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+        }
+        return false;
+    }
+}
---
+++ mobile/src/main/java/com/riceknight/sunshield/ConnectionDetector2.java
@@ -0,0 +1,8 @@
+package com.riceknight.sunshield;
+
+/**
+ * Created by ricelocal on 8/01/2015.
+ */
+public class ConnectionDetector2 {
+    
+}
