d06ec17cc11c8435c69e96269e89ae50729211df
riceknight <kitkelly@gmail.com>
2015-01-11 20:23:27 +1100

    Added the following, activity_main.xml added WIFI and MOBILE to strings.xml

--- mobile/src/main/java/com/riceknight/sunshield/ConnectionDetector.java
+++ mobile/src/main/java/com/riceknight/sunshield/ConnectionDetector.java
@@ -2,7 +2,6 @@ package com.riceknight.sunshield;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
 
 /**
@@ -16,7 +15,22 @@ public class ConnectionDetector {
         this._context = context;
     }
 
-    public boolean isConnected() {
+    public boolean isConnectedWifi() {
+        ConnectivityManager cm = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null)
+        {
+            NetworkInfo[] info = cm.getAllNetworkInfo();
+            if (info != null)
+                for(int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+        }
+        return false;
+    }
+
+    public boolean isConnectedMobile() {
         ConnectivityManager cm = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (cm != null)
         {
--- mobile/src/main/java/com/riceknight/sunshield/ConnectionDetector2.java
+++ mobile/src/main/java/com/riceknight/sunshield/ConnectionDetector2.java
@@ -1,8 +1,34 @@
 package com.riceknight.sunshield;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by ricelocal on 8/01/2015.
  */
 public class ConnectionDetector2 {
-    
+
+    private Context context;
+
+    public ConnectionDetector2(Context context) {
+        this.context = context;
+    }
+
+    private boolean isConnected() {
+        boolean isConnectedWifi = false;
+        boolean isConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    isConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    isConnectedMobile = true;
+        }
+        return isConnectedWifi || isConnectedMobile;
+    }
 }
